96102204c1861664dc6acaa7b4e673de8187f690
Amila Ariyarathna <amila03ac@gmail.com>
2025-08-22 13:43:48 +1000

    Implement fetching arnie quotes

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,27 @@
+'use strict';
+
 const { httpGet } = require('./mock-http-interface');
+const HTTP_OK = 200;
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  // We can use parallelism as there are no rate limits
+  const promises = urls.map((url) => getArnieQuote(url));
+  const results = await Promise.all(promises);
+
+  return results;
+};
+
+const getArnieQuote = async (url) => {
+  const response = await httpGet(url);
+  try {
+    const body = JSON.parse(response.body);
+
+    return response.status === HTTP_OK
+      ? { 'Arnie Quote': body.message }
+      : { FAILURE: body.message };
+  } catch (error) {
+    return { FAILURE: `Invalid JSON response from ${url}: ${error.message}` };
+  }
 };
 
 module.exports = {
